95b8f355ce5b1a136429e289d639bde1f5951e5c
Kim Seongmin <sminggo5@naver.com>
2024-01-03 21:04:07 +0900

    init

---
+++ api/src/test/java/com/seongmink/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seongmink.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
