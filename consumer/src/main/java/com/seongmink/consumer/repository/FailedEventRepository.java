0553678b0d053028cb22a31479d5ab1e8947a6eb
Kim Seongmin <sminggo5@naver.com>
2024-01-07 22:35:42 +0900

    로깅 및 실패 시 재 처리 대상 데이터 생성

---
+++ consumer/src/main/java/com/seongmink/consumer/repository/FailedEventRepository.java
@@ -0,0 +1,7 @@
+package com.seongmink.consumer.repository;
+
+import com.seongmink.consumer.domain.FailedEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FailedEventRepository extends JpaRepository<FailedEvent, Long> {
+}
